08d8b07d5c0d103ebf24e0b9a2b96b2cf039c573
JevinD <32504861+jevind@users.noreply.github.com>
2018-11-29 19:10:31 -0700

    Fragment pages working.

--- app/src/main/java/net/jevindement/collegeletgo/MainActivity.java
+++ app/src/main/java/net/jevindement/collegeletgo/MainActivity.java
@@ -5,46 +5,54 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-    private BottomNavigationItemView account;
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_sell:
-                    mTextMessage.setText(R.string.title_sell);
-                    return true;
-                case R.id.navigation_account:
-                    mTextMessage.setText(R.string.title_account);
-
-            }
-            return false;
-        }
-    };
+public class MainActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-
+        loadFragment(new HomeFragment());
     }
+    private boolean loadFragment(Fragment fragment)
+    {
+        if(fragment != null)
+        {
+           getSupportFragmentManager().beginTransaction()
+                   .replace(R.id.fragment_container,fragment)
+                   .commit();
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+    {
+        Fragment fragment = null;
+        switch (menuItem.getItemId())
+        {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
 
+            case R.id.navigation_sell:
+                fragment = new SellFragment();
+                break;
+            case R.id.navigation_account:
+                fragment = new AccountFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
 }
